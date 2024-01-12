2fbf5d34d249a270e10d09d4d38ca5b5a76675cb
Wayne Omoga <wayneomoga@Waynes-MacBook-Pro.local>
2024-01-12 15:11:14 +0300

    Finalizing UI

---
+++ src/core/downloader/save.ts
@@ -0,0 +1,69 @@
+import fs, { exists, mkdirSync } from "fs";
+import got from "got";
+import stream from "stream";
+import { promisify } from "util";
+import { homedir } from "os";
+import OS from "opensubtitles-api";
+import process from "process";
+
+
+// create an opensub api client
+const openSubs = new OS({
+    useragent: "UserAgent",
+    ssl: true,
+});
+
+const pipeline = promisify(stream.pipeline); //turn the stream pipeline to a promise.
+
+/**
+ * getURL(name,season,episode,language) => List<String>
+ * Retrieves subtitle download url from Opensubs
+ * Returns list of url and name
+ **/
+async function getURL(
+    name: string,
+    season: string,
+    episode: string,
+    language: string
+) {
+    const response: { url: { url: string; filename: string } } =
+        await openSubs.search({
+            sublanguageid: language,
+            season: season,
+            episode: episode,
+            extensions: ["srt", "vtt"],
+            limit: "best",
+            query: name,
+            gzip: true,
+        });
+    const [url] = Object.values(response);
+
+    return [url.url, url.filename];
+}
+
+
+/**
+ * getURL(filename,url) => Boolean
+ * Retrieves subtitle file from download url via a stream and saves to  file.
+ **/
+const getFile = async(filename:string, url:string) => {
+
+	const cwd = process.cwd()
+	const path = cwd.split('/').slice(0,-2).join('/').concat('/assets/subtitles');
+
+
+    await fs.writeFile(path, "", () => {
+        pipeline(
+            got.stream(url), // gets subtitle file as stream from url
+            fs.createWriteStream(path) // writes to file via a writestream
+        )
+            .then((result) => {
+                return true;
+            })
+            .catch((e) => console.log(e));
+    });
+
+    return false;
+}
+
+export { getFile, getURL };
